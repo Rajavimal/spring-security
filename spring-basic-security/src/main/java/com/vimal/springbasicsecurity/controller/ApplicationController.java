0de3d7e44f1c8b8c67fc72329542b3c4f2da5790
Raja Vimal <rajavimalseelan98@gmail.com>
2022-06-10 22:11:40 +0530

    basic security

---
+++ spring-basic-security/src/main/java/com/vimal/springbasicsecurity/controller/ApplicationController.java
@@ -0,0 +1,16 @@
+package com.vimal.springbasicsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest/auth")
+public class ApplicationController {
+
+	@GetMapping("/getMessage")
+	public String greeting() {
+		
+		return "Spring Security Example";
+	}
+}
---
+++ spring-basic-security/src/test/java/com/vimal/springbasicsecurity/SpringBasicSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vimal.springbasicsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
